ac493f9ff188026fbc16d71923ab3800b38123ce
YukthiManawat <159911127+YukthiManawat@users.noreply.github.com>
2024-02-22 19:06:27 +0530

    Checking Inventory

---
+++ ForLoopTask4B.js
@@ -0,0 +1,17 @@
+function checkInventory(inventoryList) {
+    for (let i = 0; i < inventoryList.length; i++) {
+      const currentItem = inventoryList[i];
+      const itemName = currentItem[0];
+      const stockQuantity = currentItem[1];
+      console.log(Item ${itemName} has ${stockQuantity} in stock.);
+    }
+  }
+
+const inventoryList = [
+    ['Apples', 750],
+    ['Bananas', 400],
+    ['Cherries', 225],
+    ['Watermelon', 100]
+];
+  
+checkInventory(inventoryList);
